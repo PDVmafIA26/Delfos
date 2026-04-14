08371a6bc1067353e7acf99c9d0a2ce54cdbc015
RMTorrabadella04 <raul.martin.torrabadella@gmail.com>
2026-04-14 18:34:35 +0200

    Protipo of send Messages

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from discord-delfosbot!")
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-if __name__ == "__main__":
-    main()
+@bot.event
+async def on_ready():
+    print(f"Bot conectado como {bot.user}")
+    
+    canal_id = id  # ← ID del canal aquí
+    canal = bot.get_channel(canal_id)
+    
+    if canal:
+        await canal.send("¡Hola! Estoy online 🟢")
+
+bot.run("token")
