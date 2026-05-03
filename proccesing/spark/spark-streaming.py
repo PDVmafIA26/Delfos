d3dc598d513826e1213b917619007e5ecbe74ab1
Adrián García García <adriacarlos2004@gmail.com>
2026-05-03 23:37:52 +0200

    Spark-streaming event and market adapted to new DB

--- proccesing/spark/spark-streaming.py
+++ proccesing/spark/spark-streaming.py
@@ -1,69 +1,110 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from pyspark.sql.functions import current_timestamp
 import psycopg2
+from psycopg2.extras import execute_batch
 
-# SPARK SESSION
+# Spark Session
 spark = SparkSession.builder \
     .appName("PolymarketStreaming") \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
 
-# SCHEMA
+# Schema
 schema = StructType([
     StructField("id", StringType(), True),
+    StructField("title", StringType(), True),
+    StructField("slug", StringType(), True),
+    StructField("image", StringType(), True),
+    StructField("active", BooleanType(), True),
+    StructField("liquidity", DoubleType(), True),
+    StructField("volume", DoubleType(), True),
+    StructField("volume24hr", DoubleType(), True),
+    StructField("volume1wk", DoubleType(), True),
+    StructField("volume1mo", DoubleType(), True),
+    StructField("volume1yr", DoubleType(), True),
     StructField("markets", ArrayType(
         StructType([
             StructField("id", StringType(), True),
+            StructField("conditionId", StringType(), True),
+            StructField("slug", StringType(), True),
             StructField("question", StringType(), True),
-            StructField("description", StringType(), True),
-            StructField("volume", StringType(), True),
-            StructField("category", StringType(), True),
+            StructField("image", StringType(), True),
             StructField("liquidity", StringType(), True),
+            StructField("volume", StringType(), True),
+            StructField("volume24hr", DoubleType(), True),
+            StructField("volume1wk", DoubleType(), True),
+            StructField("volume1mo", DoubleType(), True),
+            StructField("volume1yr", DoubleType(), True),
+            StructField("outcomes", StringType(), True),
             StructField("outcomePrices", StringType(), True)
         ])
     ), True)
 ])
 
-# KAFKA SOURCE
+# Kafka source
 df_raw = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:29092") \
     .option("subscribe", "events") \
     .option("startingOffsets", "earliest") \
+    .option("maxOffsetsPerTrigger", 50) \
     .load()
 
 df = df_raw.selectExpr("CAST(value AS STRING) as json")
 
 df = df.select(from_json(col("json"), schema).alias("data"))
 
-# EXPLODE MARKETS
-df = df.selectExpr("data.id as event_id", "explode(data.markets) as market")
+# Explode Markets
+df = df.select(col("data.*")).withColumn("market", explode("markets"))
 
-df = df.select(
-    col("market.id").alias("market_id"),
+# events
+events_df = df.select(
+    col("id").alias("eventid"),
+    col("title").alias("question"),
+    col("slug"),
+    col("image"),
+    col("active"),
+    col("liquidity"),
+    col("volume"),
+    col("volume24hr"),
+    col("volume1wk"),
+    col("volume1mo"),
+    col("volume1yr")
+).dropDuplicates(["eventid"])
+
+# markets
+markets_df = df.select(
+    col("id").alias("eventid"),
+    col("market.id").alias("id"),
+    col("market.conditionId").alias("conditionid"),
+    col("market.slug"),
     col("market.question"),
-    col("market.description"),
-    col("market.volume"),
-    col("market.liquidity"),
-    col("market.outcomePrices")
+    col("market.image"),
+    col("market.liquidity").cast("double"),
+    col("market.volume").cast("double"),
+    col("market.volume24hr"),
+    col("market.volume1wk"),
+    col("market.volume1mo"),
+    col("market.volume1yr")
 )
 
-# CLEANING
-df_clean = df \
-    .withColumn("volume", col("volume").cast("double")) \
-    .withColumn("liquidity", col("liquidity").cast("double")) \
-    .withColumn("timestamp", current_timestamp()) \
-    .withColumn("prices_array", from_json(col("outcomePrices"), ArrayType(StringType()))) \
-    .withColumn("price", col("prices_array")[0].cast("double")) \
-    .filter(col("volume").isNotNull()) \
-    .filter(col("price").isNotNull())
+# Outcome Tokens
+outcomes_df = df \
+    .withColumn("outcomes_array", from_json(col("market.outcomes"), ArrayType(StringType()))) \
+    .withColumn("prices_array", from_json(col("market.outcomePrices"), ArrayType(StringType()))) \
+    .withColumn("zipped", arrays_zip("outcomes_array", "prices_array")) \
+    .withColumn("exploded", explode("zipped")) \
+    .select(
+        col("market.conditionId").alias("condition_id"),
+        col("exploded.outcomes_array").alias("outcome_name"),
+        col("exploded.prices_array").cast("double").alias("price")
+    )
 
-# POSTGRES WRITER (RAW DATA)
+# Write function
 def write_to_postgres(batch_df, batch_id):
-    print(f"Writing batch {batch_id}")
+    print(f"Batch {batch_id}")
 
     if batch_df.isEmpty():
         return
@@ -74,42 +115,71 @@ def write_to_postgres(batch_df, batch_id):
         user="postgres",
         password="postgres"
     )
-
     cursor = conn.cursor()
 
-    for row in batch_df.collect():
+    # Events
+    events = events_df.collect()
+    execute_batch(cursor, """
+        INSERT INTO eventos (
+            eventid, question, slug, image, active,
+            liquidity, volume, volume24h, volume1w, volume1mo, volume1yr
+        )
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        ON CONFLICT (eventid) DO UPDATE SET
+            question = EXCLUDED.question,
+            liquidity = EXCLUDED.liquidity,
+            volume = EXCLUDED.volume
+    """, [
+        (
+            r.eventid, r.question, r.slug, r.image, r.active,
+            r.liquidity, r.volume, r.volume24hr,
+            r.volume1wk, r.volume1mo, r.volume1yr
+        ) for r in events
+    ])
 
-        cursor.execute("""
-            INSERT INTO MERCADOS_MASTER (
-                market_id,
-                question,
-                total_volume_usd,
-                liquidity
-            )
-            VALUES (%s,%s,%s,%s)
-            ON CONFLICT (market_id)
-            DO UPDATE SET
-                volume = EXCLUDED.volume,
-                liquidity = EXCLUDED.liquidity
-        """, (
-            row.market_id,
-            row.question,
-            row.volume,
-            row.liquidity
-        ))
+    # Markets
+    markets = markets_df.collect()
+    execute_batch(cursor, """
+        INSERT INTO mercados_master (
+            id, conditionid, slug, question, image,
+            liquidity, volume, volume24h, volume1w, volume1mo, volume1yr, eventid
+        )
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        ON CONFLICT (id) DO UPDATE SET
+            volume = EXCLUDED.volume,
+            liquidity = EXCLUDED.liquidity
+    """, [
+        (
+            r.id, r.conditionid, r.slug, r.question, r.image,
+            r.liquidity, r.volume, r.volume24hr,
+            r.volume1wk, r.volume1mo, r.volume1yr, r.eventid
+        ) for r in markets
+    ])
 
-        # Faltaría hacer el insert de los tokens y revisar este último insert
+    # outcome tokens
+    outcomes = outcomes_df.collect()
+    execute_batch(cursor, """
+        INSERT INTO outcome_tokens (
+            condition_id, outcome_name, price
+        )
+        VALUES (%s,%s,%s)
+    """, [
+        (
+            r.condition_id, r.outcome_name, r.price
+        ) for r in outcomes
+    ])
 
     conn.commit()
     cursor.close()
     conn.close()
 
-# STREAM START
-query = df_clean.writeStream \
+# Stream Start
+query = df.writeStream \
     .outputMode("append") \
     .foreachBatch(write_to_postgres) \
     .option("checkpointLocation", "/tmp/spark-checkpoints") \
     .start()
+
 
 print("Streaming started...")
 
